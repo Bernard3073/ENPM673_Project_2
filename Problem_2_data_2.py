4db9371a9688dbb33d1830172c423d9d6966ce59
Bernard3073 <bert41003@gmail.com>
2021-04-04 01:44:31 -0400

    Replace the Sobel_x filter with the filter to detect the yellow lane at left and white lane at right, and Add Canny Edge detection after that.

--- Problem_2_data_2.py
+++ Problem_2_data_2.py
@@ -36,47 +36,51 @@ def adjust_gamma(image, gamma=1.0):
     return cv2.LUT(image, table)
 
 
-def apply_sobel_x(gray):
-    # Apply sobel in x direction
-    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
-    # abs_sobelx = np.absolute(sobel_x)
+def extract_lane(img):
+    hsl_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
 
-    # Scale result to 0-255
-    scaled_sobel = np.uint8(255 * abs(sobel_x) / np.max(abs(sobel_x)))
-    sx_binary = np.zeros_like(scaled_sobel)
+    # Search for the yellow lane at left
+    lower_mask_yellow = np.array([20, 120, 80], dtype='uint8')
+    upper_mask_yellow = np.array([45, 200, 255], dtype='uint8')
+    mask_yellow = cv2.inRange(hsl_img, lower_mask_yellow, upper_mask_yellow)
 
-    # Keep only derivative values that are in the margin of interest
-    sx_binary[(scaled_sobel >= 100) & (
-            scaled_sobel <= 255)] = 1 * 255  # Remember to multiply by 255, if you want to show the img
+    yellow_lane = cv2.bitwise_and(hsl_img, hsl_img, mask=mask_yellow).astype(np.uint8)
 
-    # # Detect pixels that are white in the grayscale image
-    # white_binary = np.zeros_like(gray)
-    # white_binary[(gray > 200) & (gray <= 255)] = 1*255
+    # Search for the white lane at right
+    lower_mask_white = np.array([0, 200, 0], dtype='uint8')
+    upper_mask_white = np.array([255, 255, 255], dtype='uint8')
+    mask_white = cv2.inRange(hsl_img, lower_mask_white, upper_mask_white)
 
-    # binary = cv2.bitwise_or(sx_binary, white_binary)
-    # cv2.imshow("s_x", sx_binary)
+    white_lane = cv2.bitwise_and(hsl_img, hsl_img, mask=mask_white).astype(np.uint8)
 
-    return sx_binary
+    # Combine both
+    yellow_white_lane = cv2.bitwise_or(yellow_lane, white_lane)
+
+    img = cv2.cvtColor(yellow_white_lane, cv2.COLOR_HLS2BGR)
+
+    return img
 
 
-def hist_lane_pixel(binary):
+def hist_lane_pixel(warp):
     # Calculating the histogram to get max value
-    hist = np.sum(binary, axis=0)
-
+    hist = np.sum(warp, axis=0)
+    img = np.dstack((warp, warp, warp))*255
+    # cv2.imshow('im', img)
     mid_point = int(hist.shape[0] // 2)
     left_x_ind = np.argmax(hist[:mid_point])
     right_x_ind = np.argmax(hist[mid_point:]) + mid_point  # Add mid_point to avoid bias for 0
 
-    img_center = int(binary.shape[1] / 2)
+    img_center = int(warp.shape[1] / 2)
+
     turn = turn_prediction(left_x_ind, right_x_ind, img_center)
 
     # Use the sliding window method to extract pixels
     num_window = 10
     # the margin of the width of the window
     margin = 50
-    window_height = int(binary.shape[0] // num_window)
+    window_height = int(warp.shape[0] // num_window)
     # x, y positions of none zero pixels
-    nonzero_pts = binary.nonzero()
+    nonzero_pts = warp.nonzero()
     nonzero_x = np.array(nonzero_pts[1])
     nonzero_y = np.array(nonzero_pts[0])
 
@@ -91,8 +95,8 @@ def hist_lane_pixel(binary):
 
     for i in range(num_window):
         # Set the window boundary in x and y, left and right
-        window_y_low = binary.shape[0] - (i + 1) * window_height
-        window_y_high = binary.shape[0] - i * window_height
+        window_y_low = warp.shape[0] - (i + 1) * window_height
+        window_y_high = warp.shape[0] - i * window_height
         window_left_low = left_x_cur - margin
         window_left_high = left_x_cur + margin
         window_right_low = right_x_cur - margin
@@ -128,28 +132,36 @@ def hist_lane_pixel(binary):
     right_x = nonzero_x[right_lane]
     right_y = nonzero_y[right_lane]
 
-    return left_x, left_y, right_x, right_y, turn
+    img[nonzero_y[left_lane], nonzero_x[left_lane]] = [255, 0, 0]  # Blue
+    img[nonzero_y[right_lane], nonzero_x[right_lane]] = [0, 0, 255]  # Red
+    # cv2.imshow('img', img)
+    return img, left_x, left_y, right_x, right_y, turn
 
 
-def poly_fit(sx_binary, left_x, left_y, right_x, right_y):
+def poly_fit(img, left_x, left_y, right_x, right_y):
     # Use np.polyfit() to fit a second order polynomial
     left_fit = np.polyfit(left_y, left_x, 2)
     right_fit = np.polyfit(right_y, right_x, 2)
 
-    plot_y = np.linspace(0, sx_binary.shape[0] - 1, sx_binary.shape[0])
+    plot_y = np.linspace(0, img.shape[0] - 1, img.shape[0])
 
     left_fit_x = left_fit[0] * plot_y ** 2 + left_fit[1] * plot_y + left_fit[2]
     right_fit_x = right_fit[0] * plot_y ** 2 + right_fit[1] * plot_y + right_fit[2]
 
     # Plot the result in sx_binary image
-    img = np.dstack((sx_binary, sx_binary, sx_binary)) * 255
+    # img = np.dstack((img, img, img)) * 255
+
     # Extract points from line fitting
     left_pts = np.array([(np.vstack([left_fit_x, plot_y])).T])
+    # flip the array upside down in order to cv2.fillPoly()
     right_pts = np.array([np.flipud((np.vstack([right_fit_x, plot_y])).T)])
 
     pts = np.hstack((left_pts, right_pts))
     pts = np.array(pts, dtype='int32')
 
+    # turn_prediction(left_pts, right_pts)
+
+    img = np.zeros_like(img).astype(np.uint8)
     cv2.fillPoly(img, pts, Green)
     cv2.polylines(img, np.int32([left_pts]), isClosed=False, color=Blue, thickness=10)
     cv2.polylines(img, np.int32([right_pts]), isClosed=False, color=Blue, thickness=10)
@@ -157,16 +169,17 @@ def poly_fit(sx_binary, left_x, left_y, right_x, right_y):
     return img
 
 
-def turn_prediction(left_x, right_x, center_img):
-    mean_distance_x = left_x + (right_x - left_x) / 2
+def turn_prediction(right_lane_pts, left_lane_pts, image_center):
+    center_lane = left_lane_pts + (right_lane_pts - left_lane_pts) / 2
 
-    center_offset = center_img - mean_distance_x
-    if center_offset > 0:
-        return "Right"
-    elif center_offset < 0:
-        return "left"
-    elif center_offset > 8:
+    if center_lane - image_center < 0:
+        return "Turning Left"
+
+    elif center_lane - image_center < 8:
         return "Straight"
+
+    else:
+        return "Turning Right"
 
 
 def main():
@@ -191,30 +204,34 @@ def main():
 
             # cv2.polylines(img, [pts_src], True, Red)
 
-            blur = cv2.GaussianBlur(frame, (7, 7), 0)
-
+            img = undistort_image(frame)
+            # blur = cv2.GaussianBlur(undistort_img, (7, 7), 0)
+            #
             # Gamma correction to adjust lighting condition
-            hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+            hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
             clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(16, 16))
             clahe_img = clahe.apply(hsv[:, :, 2])
 
             gamma_img = adjust_gamma(clahe_img, 1.0)
             hsv[:, :, 2] = gamma_img
 
-            processed_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+            # cv2.imshow('g', img)
+            img = extract_lane(img)
 
             h, _ = cv2.findHomography(pts_src, pts_dst)
-            # warp = cv2.warpPerspective(img, h, (width, height))
-            undistort_img = undistort_image(processed_img)
-            warp = cv2.warpPerspective(undistort_img, h, (width, height))
-            # Edge Detection
-            gray = cv2.cvtColor(warp, cv2.COLOR_BGR2GRAY)
-            blur = cv2.GaussianBlur(gray, (5, 5), 0)
-            # edge = cv2.Canny(blur, 200, 300)
 
-            sx_binary = apply_sobel_x(blur)
-            left_x, left_y, right_x, right_y, turn = hist_lane_pixel(sx_binary)
-            lane_detect_img = poly_fit(sx_binary, left_x, left_y, right_x, right_y)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Filter noise
+            img_blur = cv2.bilateralFilter(gray, 9, 120, 100)
+
+            # Apply edge detection
+            img_edge = cv2.Canny(img_blur, 100, 200)
+            warp = cv2.warpPerspective(img_edge, h, (width, height))
+
+            cv2.imshow('t', warp)
+            img, left_x, left_y, right_x, right_y, turn = hist_lane_pixel(warp)
+            lane_detect_img = poly_fit(img, left_x, left_y, right_x, right_y)
 
             # Unwarp the image
             h_inv = np.linalg.inv(h)
@@ -224,15 +241,14 @@ def main():
 
             cv2.putText(final_img, turn, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.5, Red, 2, cv2.LINE_AA)
 
-            # cv2.imshow('original', frame)
-            cv2.imshow('s', sx_binary)
-            cv2.imshow('lane_detect', lane_detect_img)
-
+            # # cv2.imshow('original', frame)
+            # cv2.imshow('lane_detect', lane_detect_img)
+            #
             # cv2.imshow('warp', warp)
-            # cv2.imshow('undistort', undistort_img)
-            # cv2.imshow('edge', edge)
-            # cv2.setMouseCallback('f', click_event)
-            # cv2.imshow('final', final_img)
+            # # cv2.imshow('undistort', undistort_img)
+            # # cv2.imshow('edge', edge)
+            # # cv2.setMouseCallback('f', click_event)
+            cv2.imshow('final', final_img)
 
             if cv2.waitKey(30) & 0xFF == ord("q"):
                 break
