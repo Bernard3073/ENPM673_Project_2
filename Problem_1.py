464e230ff8d16ff20ae8b76a1a477eb5305caf14
Bernard3073 <bert41003@gmail.com>
2021-07-01 16:44:23 -0400

    Add comments

--- Problem_1.py
+++ Problem_1.py
@@ -21,10 +21,11 @@ def adjust_gamma(image, gamma=1.0):
 
 
 def main():
+    # https://www.pyimagesearch.com/2021/02/01/opencv-histogram-equalization-and-adaptive-histogram-equalization-clahe/
     cap = cv2.VideoCapture('./Night Drive - 2689.mp4')
-    # a = np.zeros((256,),dtype=np.float16)
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # out = cv2.VideoWriter('Night_Drive_improved.avi', fourcc, 20.0, (1920, 1080))
+    a = np.zeros((256,),dtype=np.float16)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('Night_Drive_improved.avi', fourcc, 20.0, (1920, 1080))
     if not cap.isOpened():
         print("Error")
     while cap.isOpened():
@@ -32,9 +33,11 @@ def main():
         if ret:
             height, width, _ = frame.shape
             blur = cv2.GaussianBlur(frame, (7, 7), 0)
+            # Convert the color image to HSV, in order to apply histogram equalization method to the "V" channel
             hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-
+            # Instantiate the CLAHE algorithm using cv2.createCLAHE()
             clahe = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(16, 16))
+            # Call the .apply method on the CLAHE object to apply histogram equalization
             clahe_img = clahe.apply(hsv[:, :, 2])
             
             gamma_img = adjust_gamma(clahe_img, 1.0)
@@ -42,32 +45,17 @@ def main():
 
             processed_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
-            # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # hist,bins = np.histogram(hsv[:, :, 0].flatten(),256,[0,256])
-            # cdf = hist.cumsum()
-            # cdf_normalized = cdf * float(hist.max()) / cdf.max()
-            
-            # cdf_m = np.ma.masked_equal(cdf,0)
-            # cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
-            # cdf = np.ma.filled(cdf_m,0).astype('uint8')
-            # processed_img = cdf[hsv]
-            
-            # # equalize the histogram of the Y channel
-            # hsv[:,:,0] = cv2.equalizeHist(hsv[:,:,0])
-
-            # # convert the YUV image back to RGB format
-            # processed_img = cv2.cvtColor(hsv, cv2.COLOR_YUV2BGR)
 
             # showing the image
             cv2.imshow('improved_image', processed_img)
-            # out.write(processed_img)
+            out.write(processed_img)
             if cv2.waitKey(30) & 0xFF == ord("q"):
                 break
         else:
             break
 
     cap.release()
-    # out.release()
+    out.release()
     cv2.destroyAllWindows()
 
 
